a2ca0aeaa3567769c5be0c65a852eef8f43342f1
Ahan Sen <senahan003@gmail.com>
2021-06-04 18:15:40 +0530

    final commit

--- client/src/context/ConversationsProvider.js
+++ client/src/context/ConversationsProvider.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect, useCallback } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
-import { useContacts } from "../context/ContactsProvider";
-import { useSocket } from "../context/SocketProvider";
+import { useContacts } from "./ContactsProvider";
+import { useSocket } from "./SocketProvider";
 
 const ConversationsContext = React.createContext();
 
@@ -14,9 +14,8 @@ export function ConversationsProvider({ id, children }) {
     "conversations",
     []
   );
-
-  const { contacts } = useContacts();
   const [selectedConversationIndex, setSelectedConversationIndex] = useState(0);
+  const { contacts } = useContacts();
   const socket = useSocket();
 
   function createConversation(recipients) {
@@ -24,28 +23,6 @@ export function ConversationsProvider({ id, children }) {
       return [...prevConversations, { recipients, messages: [] }];
     });
   }
-
-  const formattedConversations = conversations.map((conversation, index) => {
-    const recipients = conversation.recipients.map((recipient) => {
-      const contact = contacts.find((contact) => {
-        return contact.id === recipient;
-      });
-      const name = (contact && contact.name) || recipient;
-      return { id: recipient, name };
-    });
-
-    const messages = conversation.messages.map((message) => {
-      const contact = contacts.find((contact) => {
-        return contact.id === message.sender;
-      });
-      const name = (contact && contact.name) || message.sender;
-      const fromMe = id === message.sender;
-      return { ...message, senderName: name, fromMe };
-    });
-
-    const selected = index === selectedConversationIndex;
-    return { ...conversation, messages, recipients, selected };
-  });
 
   const addMessageToConversation = useCallback(
     ({ recipients, text, sender }) => {
@@ -75,28 +52,52 @@ export function ConversationsProvider({ id, children }) {
   );
 
   useEffect(() => {
-    if (socket == null) return;
+    if (socket == null) {
+      console.log("null");
+      return;
+    }
 
-    socket.on("recieve-message", addMessageToConversation);
-    return () => socket.off("recieve-message");
+    socket.on("receive-message", addMessageToConversation);
+    return () => socket.off("receive-message");
   }, [socket, addMessageToConversation]);
 
   function sendMessage(recipients, text) {
     socket.emit("send-message", { recipients, text });
-
     addMessageToConversation({ recipients, text, sender: id });
   }
 
+  const formattedConversations = conversations.map((conversation, index) => {
+    const recipients = conversation.recipients.map((recipient) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === recipient;
+      });
+      const name = (contact && contact.name) || recipient;
+      return { id: recipient, name };
+    });
+
+    const messages = conversation.messages.map((message) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === message.sender;
+      });
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+
+    const selected = index === selectedConversationIndex;
+    return { ...conversation, messages, recipients, selected };
+  });
+
+  const value = {
+    conversations: formattedConversations,
+    selectedConversation: formattedConversations[selectedConversationIndex],
+    sendMessage,
+    selectConversationIndex: setSelectedConversationIndex,
+    createConversation,
+  };
+
   return (
-    <ConversationsContext.Provider
-      value={{
-        conversations: formattedConversations,
-        createConversation,
-        sendMessage,
-        selectedConversation: formattedConversations[selectedConversationIndex],
-        selectConversationIndex: setSelectedConversationIndex,
-      }}
-    >
+    <ConversationsContext.Provider value={value}>
       {children}
     </ConversationsContext.Provider>
   );
