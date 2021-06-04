dee3dc1034f422ba00ab0aeb40e6bd3262bef4a8
Ahan Sen <senahan003@gmail.com>
2021-06-04 14:17:34 +0530

    display messages

--- client/src/context/ConversationsProvider.js
+++ client/src/context/ConversationsProvider.js
@@ -8,7 +8,7 @@ export function useConversations() {
   return useContext(ConversationsContext);
 }
 
-export function ConversationsProvider({ children }) {
+export function ConversationsProvider({ id, children }) {
   const [conversations, setConversations] = useLocalStorage(
     "conversations",
     []
@@ -31,15 +31,54 @@ export function ConversationsProvider({ children }) {
       const name = (contact && contact.name) || recipient;
       return { id: recipient, name };
     });
+
+    const messages = conversation.messages.map((message) => {
+      const contact = contacts.find((contact) => {
+        return contact.id === message.sender;
+      });
+      const name = (contact && contact.name) || message.sender;
+      const fromMe = id === message.sender;
+      return { ...message, senderName: name, fromMe };
+    });
+
     const selected = index === selectedConversationIndex;
-    return { ...conversation, recipients, selected };
+    return { ...conversation, messages, recipients, selected };
   });
+
+  const addMessageToConversation = ({ recipients, text, sender }) => {
+    setConversations((prevConversations) => {
+      let madeChange = false;
+      const newMessage = { sender, text };
+      const newConversations = prevConversations.map((conversation) => {
+        if (arrayEquality(conversation.recipients, recipients)) {
+          madeChange = true;
+          return {
+            ...conversation,
+            messages: [...conversation.messages, newMessage],
+          };
+        }
+
+        return conversation;
+      });
+
+      if (madeChange) {
+        return newConversations;
+      } else {
+        return [...prevConversations, { recipients, messages: [newMessage] }];
+      }
+    });
+  };
+
+  function sendMessage(recipients, text) {
+    addMessageToConversation({ recipients, text, sender: id });
+  }
 
   return (
     <ConversationsContext.Provider
       value={{
         conversations: formattedConversations,
         createConversation,
+        sendMessage,
         selectedConversation: formattedConversations[selectedConversationIndex],
         selectConversationIndex: setSelectedConversationIndex,
       }}
@@ -47,4 +86,15 @@ export function ConversationsProvider({ children }) {
       {children}
     </ConversationsContext.Provider>
   );
+}
+
+function arrayEquality(a, b) {
+  if (a.length !== b.length) return false;
+
+  a.sort();
+  b.sort();
+
+  return a.every((element, index) => {
+    return element === b[index];
+  });
 }
